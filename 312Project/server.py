fcd4604668be7db97a6945da8436ef83a7bd4361
Junstinr <78913628+Junstinr@users.noreply.github.com>
2023-04-26 18:16:20 -0400

    later

--- 312Project/server.py
+++ 312Project/server.py
@@ -12,7 +12,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('login.html')
+
+@app.route("/login")
+def login():
+    score = 100
+    return render_template('login.html', score = score)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
